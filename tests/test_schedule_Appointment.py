3ccb08dc7119754c20635025dfe28feb39749571
David White <david.l.white.git@gmail.com>
2019-07-06 08:55:07 -0400

    Add test for calc_prior

--- tests/test_schedule_Appointment.py
+++ tests/test_schedule_Appointment.py
@@ -2,8 +2,12 @@ from schedule import Appointment
 from tests.objects import (
     appt1,
     appt2,
-    appt3
+    appt3,
 )
+from constants import TIME_FORMAT
+from utils import Time
+import bisect
+import copy
 import unittest
 import sys
 sys.path.append('..')
@@ -39,6 +43,48 @@ class TestClass(unittest.TestCase):
                           str(self.appt.interpreter))
         actual = self.appt.brief()
         self.assertEqual(brief_str_appt, actual)
+
+        # calc_prior
+        others = [appt1, appt2, appt3]
+        appt_to_test = appt3
+        appt_idx = others.index(appt_to_test)
+        self.assertEqual(2, appt_idx)
+
+        start = [interval.start for interval in others]
+        self.assertEqual([Time("8:00", TIME_FORMAT),
+                          Time("8:25", TIME_FORMAT),
+                          Time("8:45", TIME_FORMAT)], start)
+
+        finish = [interval.finish for interval in others]
+        self.assertEqual([Time("8:10", TIME_FORMAT),
+                          Time("9:05", TIME_FORMAT),
+                          Time("9:25", TIME_FORMAT)], finish)
+
+        # overlapping marks the next interval that would overlap,
+        # meaning that interval a.finish would be > than appt_to_test.start
+        overlapping = bisect.bisect(finish, start[appt_idx])
+        self.assertEqual(1, overlapping)
+        self.assertEqual(Time("9:05", TIME_FORMAT),
+                         finish[overlapping])
+        # appt_to_test.start. In order to satisfy the condition,
+        # we need a.finish < appt_to_test.start, so we exclude
+        # the appts that aren't compatible
+        compatible_copy = copy.deepcopy(others[:overlapping])
+        self.assertEqual([appt1], compatible_copy)
+
+        # We sort the indices of compatible appts in reverse order
+        # and test them for compatibility, until one is found
+        # doing so in reverse order ensures the compatible appt is
+        # the rightmost compatible appt
+        compatible_idx = [others.index(other) for other in compatible_copy]
+        compatible_idx.sort(reverse=True)
+        self.assertEqual([0], compatible_idx)
+        rightmost_compatible_appt = others[compatible_idx[-1]]
+        self.assertTrue(rightmost_compatible_appt.is_compatible(appt_to_test))
+        self.assertEqual(appt1, appt3.calc_prior(others))
+
+        # get_prior_num
+        pass
 
         # __str__
         appt_str = ("1|08:00|10|08:10|" +
