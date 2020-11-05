ae51b05d181e79929dfa7380cda2956eaef26664
David White <dwhite0627@gmail.com>
2020-11-05 13:33:10 -0500

    Change fh for the demo data, adapt resources to the demo data

--- csvconfig.py
+++ csvconfig.py
@@ -6,7 +6,7 @@ from csvprocessor import csv_to_list
 from csvprocessor import create_appointment
 
 # Variables
-fh = 'TestData.csv'
+fh = 'DemoData.csv'
 appts = []
 appt_headers = ['Appt_ID', 'Start', 'Duration',
                 'Appt_Priority', 'Appt_Prov', 'Appt_Interpreter']
@@ -17,22 +17,23 @@ location_headers = ['LocX', 'LocY', 'Building_Name', 'Clinic_Name']
 BASE_WT = 5
 
 # Areas
-area1 = {'Building 1': BASE_WT, 'Building 3': 1,
-         'Building 2': BASE_WT, 'Building 4': 1}
-area2 = {'Building 1': 1, 'Building 3': BASE_WT,
-         'Building 2': 1, 'Building 4': 1}
-area3 = {'Building 1': 1, 'Building 3': 1,
-         'Building 2': 1, 'Building 4': BASE_WT}
-floater = {'Building 1': 1, 'Building 3': 1,
-           'Building 2': 1, 'Building 4': 1}
+area1 = {'Building 1': BASE_WT, 'Building 2': 1, 'Building 3': 1,
+         'Building 4': 1, 'Building 5': BASE_WT}
+area2 = {'Building 1': 1, 'Building 2': BASE_WT, 'Building 3': 1,
+         'Building 4': 1, 'Building 5': 1}
+area3 = {'Building 1': 1, 'Building 2': 1, 'Building 3': BASE_WT,
+         'Building 4': 1, 'Building 5': 1}
+area4 = {'Building 1': 1, 'Building 2': 1, 'Building 3': 1,
+         'Building 4': BASE_WT, 'Building 5': 1}
+floater = {'Building 1': 1, 'Building 2': 1, 'Building 3': 1,
+           'Building 4': 1, 'Building 5': 1}
 
 # Interpreters
-interp1 = Interpreter('I1', ['Spanish', 'Portuguese'],
-                      'Female', '8:00', '16:30', area1)
-interp2 = Interpreter('I2', ['Spanish'], 'Male', '8:00', '12:00', area2)
-interp3 = Interpreter('I3', ['Spanish'], 'Female', '8:30', '17:00', area3)
-interp4 = Interpreter('I4', ['Spanish'], 'Male', '8:30', '14:30', floater)
-team = [interp1, interp2, interp3, interp4]
+interp1 = Interpreter('Staff1', {'Spanish', 'Portuguese'},
+                      'Female', '8:00', '16:30', area2)
+interp2 = Interpreter('Staff1', {'Spanish', 'Portuguese'},
+                      'Female', '10:30', '16:30', floater)
+team = [interp1, interp2]
 
 # Build headers
 counter = 0
@@ -53,6 +54,5 @@ sched = Schedule(appts, team)
 
 # Compare scheduling method impact
 opt = Optimum(sched)
-opt.compare_performance(opt.schedule_methods)
 report = ConsoleReport(opt)
 report.print_comparison()
